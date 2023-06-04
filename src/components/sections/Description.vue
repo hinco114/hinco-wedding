75bbff0126cd45caf9c4a20da6df32562fd939ed
SangHyoun Yun <hainco@gmail.com>
2023-06-04 23:21:09 +0900

    Update: Description message

--- src/components/sections/Description.vue
+++ src/components/sections/Description.vue
@@ -3,8 +3,8 @@
     <p class="text-center text-6 mb-1">안내말씀</p>
     <div class="m-a h-1px w-full max-w-85px bg-black"></div>
     <div class="text-center mt-5">
-      <p>성당에서 가족끼리 예식을 진행할 예정입니다.</p>
-      <p>많은 분들의 축복은 마음으로 받겠습니다.</p>
+      <p>성당에서 가족끼리 조용히 예식을 진행할 예정입니다.</p>
+      <p></p>
     </div>
   </div>
 </template>
