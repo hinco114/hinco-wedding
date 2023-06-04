4a12f52df3813c53525f929520158e9b31e502eb
SangHyoun Yun <hainco@gmail.com>
2023-06-04 23:21:51 +0900

    Add: BankAccount

--- src/components/AppContents.vue
+++ src/components/AppContents.vue
@@ -4,8 +4,8 @@
   <Description />
   <StraightPictures />
   <Flicker />
-  <!--  <Contact />-->
   <NMap />
+  <BankAccount :bankAccounts="bankAccounts" />
   <Footer />
 </template>
 
@@ -17,5 +17,27 @@ import Footer from './sections/Footer.vue'
 import Flicker from './sections/Flicker.vue'
 import Introduce from './sections/Introduce.vue'
 import StraightPictures from './sections/StraightPictures.vue'
-import Contact from './sections/Contact.vue'
+import BankAccount from './sections/BankAccount.vue'
+
+const bankAccounts = {
+  groom: [
+    {
+      name: '윤중한',
+      account: '국민은행',
+      bank: '201-21-1109361',
+    },
+    {
+      name: '윤상현',
+      account: '신한은행',
+      bank: '380-12-149911',
+    },
+  ],
+  bride: [
+    {
+      name: '임성은',
+      account: '우리은행',
+      bank: '1002-748-145813',
+    },
+  ],
+}
 </script>
